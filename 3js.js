2d6926ac1acf514e75bfedc4ce5414d9b8251622
ZoyaMi <38980690+ZoyaMi@users.noreply.github.com>
2018-05-10 17:52:36 +0300

    Исправление замечаний
    1. Добавлены отступы
    2. Убраны лишние детали в массиве

--- 3js.js
+++ 3js.js
@@ -1,17 +1,33 @@
-var elements = [man = {name:'man',cool: 'yes', old:'yes',married: 'yes'},
-dog = {name:'dog',hair: 'short',cool: 'yes'},
-squirrel = {name:'squirrel', tall: 'yes', cool: 'yes', fast: 'yes', powerfull: 'no'}];
-for (w=0;w<elements.length;w++)
-var lim=elements.length-2;
-for (var i = lim ; i >= 0; i--) {
-for (var j = 0; j <= i; j++) {
-if (Object.keys(elements[i]).length< Object.keys(elements[i+1]).length) {
-var tmp = elements[i];
-elements[i] = elements[i+1];
-elements[i+1] = tmp;
+var elements = [{
+        name: 'man',
+        cool: 'yes',
+        old: 'yes',
+        married: 'yes'
+    },
+    {
+        name: 'dog',
+        hair: 'short',
+        cool: 'yes'
+    },
+    {
+        name: 'squirrel',
+        tall: 'yes',
+        cool: 'yes',
+        fast: 'yes',
+        powerfull: 'no'
+    }
+];
+for (w = 0; w < elements.length; w++)
+    var lim = elements.length - 2;
+for (var i = lim; i >= 0; i--) {
+    for (var j = 0; j <= i; j++) {
+        if (Object.keys(elements[i]).length < Object.keys(elements[i + 1]).length) {
+            var tmp = elements[i];
+            elements[i] = elements[i + 1];
+            elements[i + 1] = tmp;
+        }
+    }
 }
-}
-}
-for (w=0;w<elements.length;w++){
-console.log(elements[w].name +': ' + Object.keys(elements[w]).length)
+for (w = 0; w < elements.length; w++) {
+    console.log(elements[w].name + ': ' + Object.keys(elements[w]).length)
 }
