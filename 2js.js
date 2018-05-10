47974badc33b5d879e227880321dc47ba0feb27d
ZoyaMi <38980690+ZoyaMi@users.noreply.github.com>
2018-05-10 17:44:35 +0300

    Исправление замечаний: Добавление отступов

--- 2js.js
+++ 2js.js
@@ -1,16 +1,15 @@
-var elements = [1,2,3,2,5,2,2];
+var elements = [1, 2, 3, 2, 5, 2, 2];
 
-var tmp ={};
+var tmp = {};
 var max = 0;
-for (var i=0;i<elements.length;i++){
-if (tmp[elements[i]]) {
-tmp[elements[i]]++;
-}
-else {
-tmp[elements[i]]=1;
-}
-if (tmp[elements[i]]>max) {
-var result=elements[i];
-}
+for (var i = 0; i < elements.length; i++) {
+    if (tmp[elements[i]]) {
+        tmp[elements[i]]++;
+    } else {
+        tmp[elements[i]] = 1;
+    }
+    if (tmp[elements[i]] > max) {
+        var result = elements[i];
+    }
 }
 console.log("Result is: " + result);
