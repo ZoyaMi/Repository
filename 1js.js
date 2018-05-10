91722752470a2a4eace910cde199c8ccf9d75070
ZoyaMi <38980690+ZoyaMi@users.noreply.github.com>
2018-05-10 16:58:19 +0300

    Исправление замечаний
    1. Добавлены отступы
    2. Выполняется c помощью нового объекта 
    3. Вывод всего объекта в консоль

--- 1js.js
+++ 1js.js
@@ -1,13 +1,9 @@
-var dev= {
-cool: 'yes',
-stylish: 'no'
+var dev = {
+    cool: 'yes',
+    stylish: 'no'
 }
-for (i in dev){
-var tmp=i;
-var tmp2=dev[i];
-delete dev[i];
-dev[tmp2]=tmp;
+tmp = {};
+for (i in dev) {
+    tmp[dev[i]] = i;
 }
-for (i in dev){
-console.log('property: ' + i + ' | ' + 'value: ' + dev[i]);
-}
+console.log(tmp);
